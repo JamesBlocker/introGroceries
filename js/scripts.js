d02203117ffae369a990338b1db142c5c7b148a5
Alan Falcon and James Blocker <alan+james@>
2017-09-18 15:28:02 -0700

    fix output and array issue

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,43 @@
 $(document).ready(function() {
 
   $('form').submit(function(event) {
+
     var item1 = $('#order1').val();
     var item2 = $('#order2').val();
     var item3 = $('#order3').val();
     var items = [item1, item2, item3];
-    // myArrays.push("someThing");
+    var upperItems = [];
+    //var sortItems = [];
 
-    // for (var i = 0; i < myArrays.length; i++) {
-    //   $('#list').append('<li>' + myArrays[i] + '</li>');
-    //   alert(myArrays[i]);
-    //   alert(myArray)
-    // }
-
-    var upperItems = items.map(function(item) {
-      return item.toUpperCase();
-      alert(upperItems);
+// for (var index = 0; index < upperItems.length; index += 1) {
+//
+// }
+    items.forEach(function(item) {
+      upperItems.push(item.toUpperCase());
+    });
+    var sortItems = upperItems.sort();
+    sortItems.forEach(function(item) {
+      $("#list").append("<li>" + item + "</li>");
     });
 
-    event.preventDefault();
+    $('form').hide();
+
+    // items.forEach(function(item) {
+    // upperItems.push(item.toUpperCase());
+
+//    upperItems.forEach(function(item) {
+//      $("#list").append("<li>" + item + "</li>");
+
+//     sortItems.push(upperItems.sort());
+//     var upSort = function($('#list').append('<li>' + item + '</li>')) {
+//     var upSort = function($('#list').append('<li>' + item + '</li>')) {
+
+//     }
+
+//     $('#list').append('<li>' + item + '</li>');
+
+//   alert(upperItems.sort());
+
+event.preventDefault();
   });
 });
